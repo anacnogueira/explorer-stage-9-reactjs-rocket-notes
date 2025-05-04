3516302b274537a77a7879eefc410ad9332c4ac5
Ana Claudia <anacnogueira@gmail.com>
2025-05-04 18:04:02 -0300

    Creating button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,5 @@
+import { Container } from "./styles";
+
+export function Button() {
+  return <Container type="button">My button</Container>;
+}
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
-import { Container } from "./styles.js";
+import { Container } from "./styles";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
       <span>Ana Claudia Nogueira</span>
+      <Button />
     </Container>
   );
 }
