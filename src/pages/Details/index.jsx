9f2ae9a80dad8c0bb05757e6c7113a66989541db
Ana Claudia <anacnogueira@gmail.com>
2025-05-05 11:24:07 -0300

    Created Section Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,11 +1,25 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <Section title="Links Uteis">
+        <Links>
+          <li>
+            <a href="#">Item 1</a>
+          </li>
+          <li>
+            <a href="#">Item 2</a>
+          </li>
+          <li>
+            <a href="#">Item 3</a>
+          </li>
+        </Links>
+      </Section>
       <Button title="Voltar" />
     </Container>
   );
