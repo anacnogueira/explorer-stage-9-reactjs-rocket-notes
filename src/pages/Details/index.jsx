70149f20303cfbdfa78adb45c65a41b541e08354
Ana Claudia <anacnogueira@gmail.com>
2025-05-05 16:06:46 -0300

    Finalizing details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles";
+import { Container, Links, Content } from "./styles";
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
@@ -9,25 +9,36 @@ export function Details() {
   return (
     <Container>
       <Header />
-      <ButtonText title="Excluir Nota" />
-      <Section title="Links Uteis">
-        <Links>
-          <li>
-            <a href="#">Item 1</a>
-          </li>
-          <li>
-            <a href="#">Item 2</a>
-          </li>
-          <li>
-            <a href="#">Item 3</a>
-          </li>
-        </Links>
-      </Section>
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
-      <Button title="Voltar" />
+      <main>
+        <Content>
+          <ButtonText title="Excluir Nota" />
+          <h1>Introdução ao Nodejs</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit
+            iste nesciunt doloremque similique facilis labore dolore sunt? Ex
+            dolor sint consectetur repudiandae deserunt est doloribus
+            cupiditate, nam, sequi consequuntur cumque.
+          </p>
+          <Section title="Links Uteis">
+            <Links>
+              <li>
+                <a href="#">Item 1</a>
+              </li>
+              <li>
+                <a href="#">Item 2</a>
+              </li>
+              <li>
+                <a href="#">Item 3</a>
+              </li>
+            </Links>
+          </Section>
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   );
 }
