5f48cd99bafdd7b7a93888e70d71500f01bdab94
Ana Claudia <anacnogueira@gmail.com>
2025-05-05 12:27:36 -0300

    ButtonText Component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,11 +3,13 @@ import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
 import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
 
 export function Details() {
   return (
     <Container>
       <Header />
+      <ButtonText title="Excluir Nota" />
       <Section title="Links Uteis">
         <Links>
           <li>
