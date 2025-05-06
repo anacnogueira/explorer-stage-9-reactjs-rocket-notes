7c16690c583a21b6f58e748a42035f63e46783c7
Ana Claudia <anacnogueira@gmail.com>
2025-05-06 08:50:51 -0300

    Input Component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { FiPlus } from "react-icons/fi";
 import { Container, Brand, Menu, Search, Content, NewNote } from "./styles";
 import { Header } from "../../components/Header";
 import { ButtonText } from "../../components/ButtonText";
+import { Input } from "../../components/Input";
 
 export function Home() {
   return (
@@ -22,7 +23,9 @@ export function Home() {
           <ButtonText title="Node" />
         </li>
       </Menu>
-      <Search></Search>
+      <Search>
+        <Input placeholder="Pesquisar pelo título" />
+      </Search>
       <Content></Content>
       <NewNote>
         <FiPlus />
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -38,6 +38,8 @@ export const Menu = styled.ul`
 `;
 export const Search = styled.ul`
   grid-area: search;
+
+  padding: 64px 64px 0;
 `;
 export const Content = styled.div`
   grid-area: content;
