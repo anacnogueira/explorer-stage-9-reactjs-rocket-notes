c9f9a5f4135fd7b5a3bb27ceeb8724c0bf63c604
Ana Claudia <anacnogueira@gmail.com>
2025-05-07 16:16:15 -0300

    Profile Avatar

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,5 +1,5 @@
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
-import { Container, Form } from "./styles";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
+import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 
@@ -12,18 +12,19 @@ export function Profile() {
         </a>
       </header>
       <Form>
-        <Input
-          placeholder="Nome"
-          type="text"
-          icon={FiUser}
-          value="Ana Claudia Nogueira"
-        />
-        <Input
-          placeholder="E-mail"
-          type="email"
-          icon={FiMail}
-          value="anacnogueira@gmail.com"
-        />
+        <Avatar>
+          <img
+            src="https://github.com/anacnogueira.png"
+            alt="Foto do usuário"
+          />
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
+        <Input placeholder="Nome" type="text" icon={FiUser} />
+        <Input placeholder="E-mail" type="email" icon={FiMail} />
         <Input placeholder="Senha atual" type="password" icon={FiLock} />
         <Input placeholder="Nova senha" type="password" icon={FiLock} />
 
