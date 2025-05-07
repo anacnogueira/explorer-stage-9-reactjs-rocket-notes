53a9195b12396f8633652510f9ff850eebd25cdd
Ana Claudia <anacnogueira@gmail.com>
2025-05-07 10:53:08 -0300

    Add SignIn Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./styles/global";
 
@@ -10,7 +10,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </StrictMode>
 );
