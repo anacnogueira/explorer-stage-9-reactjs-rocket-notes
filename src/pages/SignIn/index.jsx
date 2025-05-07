7de4f9c893c2be67836593905612e9332d263f4b
Ana Claudia <anacnogueira@gmail.com>
2025-05-07 11:37:58 -0300

    SignIn Form

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,5 +1,20 @@
-import { Container } from "./styles";
+import { FiMail, FiLock } from "react-icons/fi";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+import { Container, Form } from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+        <h2>Faça seu login</h2>
+        <Input placeholder="e-mail" type="email" icon={FiMail} />
+        <Input placeholder="senha" type="password" icon={FiLock} />
+        <Button title="Entrar" />
+        <a href="">Criar conta</a>
+      </Form>
+    </Container>
+  );
 }
