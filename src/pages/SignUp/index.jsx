6c80dec390a7de77178f02fc5caff347eab75604
Ana Claudia <anacnogueira@gmail.com>
2025-05-08 17:44:32 -0300

    Adding navigation

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,5 @@
 import { FiUser, FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
@@ -15,7 +16,7 @@ export function SignUp() {
         <Input placeholder="E-mail" type="email" icon={FiMail} />
         <Input placeholder="Senha" type="password" icon={FiLock} />
         <Button title="Cadastrar" />
-        <a href="">Voltar para login</a>
+        <Link to="/">Voltar para login</Link>
       </Form>
     </Container>
   );
