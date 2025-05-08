8b6eb1874841e11190c81a43f59e3112ec908951
Ana Claudia <anacnogueira@gmail.com>
2025-05-08 16:42:07 -0300

    Page New fisnished

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -4,6 +4,7 @@ import { Input } from "../../components/Input";
 import { Textarea } from "../../components/Textarea";
 import { NoteItem } from "../../components/NoteItem";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
 
 export function New() {
   return (
@@ -15,13 +16,19 @@ export function New() {
             <h1>Criar Nota</h1>
             <a href="/">Voltar</a>
           </header>
-
           <Input placeholder="Título" />
           <Textarea placeholder="Observações" />
           <Section title="Links ùteis">
             <NoteItem value="https://mayacosmeticos.com.br" />
             <NoteItem isNew placeholder="Novo Link" />
           </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="reactjs" />
+              <NoteItem isNew placeholder="Nova Tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
