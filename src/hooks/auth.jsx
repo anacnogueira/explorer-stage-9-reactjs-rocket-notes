4336d138d4def52f6218a3968f15c7c2cfd404a3
Ana Claudia <anacnogueira@gmail.com>
2025-05-18 18:08:53 -0300

    update avatar user

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -45,8 +45,15 @@ export function AuthProvider({ children }) {
     setData({});
   }
 
-  async function updateProfile({ user }) {
+  async function updateProfile({ user, avatarFile }) {
     try {
+      if (avatarFile) {
+        const fileUploadForm = new FormData();
+        fileUploadForm.append("avatar", avatarFile);
+
+        const response = await api.patch("/users/avatar", fileUploadForm);
+        user.avatar = response.data.avatar;
+      }
       await api.put("/users", user);
 
       localStorage.setItem("@notes:user", JSON.stringify(user));
--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -5,6 +5,8 @@ import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 import { Container, Form, Avatar } from "./styles";
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
+import { api } from "../../services/api";
+import avatarPlaceholder from "../../assets/avatar_placeholder.svg";
 
 export function Profile() {
   const { user, updateProfile } = useAuth();
@@ -14,6 +16,12 @@ export function Profile() {
   const [passwordOld, setPasswordOld] = useState("");
   const [passwordNew, setPasswordNew] = useState("");
 
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
+  const [avatar, setAvatar] = useState(avatarUrl);
+  const [avatarFile, setAvatarFile] = useState(null);
+
   async function handleUpdate() {
     const user = {
       name,
@@ -21,7 +29,15 @@ export function Profile() {
       password: passwordNew,
       old_password: passwordOld,
     };
-    await updateProfile({ user });
+    await updateProfile({ user, avatarFile });
+  }
+
+  function handleChangeAvatar(event) {
+    const file = event.target.files[0];
+    setAvatarFile(file);
+
+    const imagePreview = URL.createObjectURL(file);
+    setAvatar(imagePreview);
   }
 
   return (
@@ -33,13 +49,10 @@ export function Profile() {
       </header>
       <Form>
         <Avatar>
-          <img
-            src="https://github.com/anacnogueira.png"
-            alt="Foto do usuário"
-          />
+          <img src={avatar} alt="Foto do usuário" />
           <label htmlFor="avatar">
             <FiCamera />
-            <input id="avatar" type="file" />
+            <input id="avatar" type="file" onChange={handleChangeAvatar} />
           </label>
         </Avatar>
 
